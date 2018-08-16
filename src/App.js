9634662371c1b0715dc6d6ccc3346bb76d1f5fd2
Jinsoo Park <jinsoopark@Jinsooui-iMac.local>
2018-08-17 00:35:33 +0900

    Chapter 01

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+
+class App extends Component {
+  render() {
+    let user = {
+      firstname: 'Jinsoo',
+      lastname: 'Park'
+    };
+    const helloWorld = '리액트에 오신 당신을 환영합니다.';
+
+    return (
+        <div className="App">
+          {list.map(item =>
+              <div key={item.objectID}>
+            <span>
+            <a href={item.url}>{item.title}</a>
+            </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+          )}
+        </div>
+    );
+  }
+}
+
+export default App;
