6e3cd8c6a9f63014df172c386b081dd5b73f556f
UrangUrang <ddwax89@gmail.com>
2018-08-17 17:13:21 +0900

    doing chapther02

--- src/App.js
+++ src/App.js
@@ -3,48 +3,71 @@ import './App.css';
 
 
 const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
 ];
 
 
 class App extends Component {
-  render() {
-    let user = {
-      firstname: 'Jinsoo',
-      lastname: 'Park'
-    };
-    const helloWorld = '리액트에 오신 당신을 환영합니다.';
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: list
+        };
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+    onDismiss(id) {
+      // function isNotId(item) {
+      //   return item.objectID !== id;
+      // }
+      // const updatedList = this.state.list.filter(isNotId);
+      // ------------------
+      // const isNotId = item => item.objectID !== id;
+      // const updatedList = this.state.list.filter(isNotId)
+      // ------------------
+        const updatedList = this.state.list.filter(item => item.objectID !== id)
+        this.setState({list: updatedList})
+    }
+    render() {
+        let user = {
+            firstname: 'Jinsoo',
+            lastname: 'Park'
+        };
 
-    return (
-        <div className="App">
-          {list.map(item =>
-              <div key={item.objectID}>
-            <span>
-            <a href={item.url}>{item.title}</a>
-            </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </div>
-          )}
-        </div>
-    );
-  }
+        return (
+            <div className="App">
+                {this.state.list.map(item =>
+                    <div key={item.objectID}>
+                  <span>
+                      <a href={item.url}>{item.title}</a>
+                  </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <div>
+                            <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                                Dismiss
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
